8c88a4e041725097d5cce2cebfebbad34f4d6c21
Mailisha Awuor <mailishaawuor@gmail.com>
2026-04-21 11:25:13 +0300

    Completed Assignment

--- Unit/aaaAssignment.js
+++ Unit/aaaAssignment.js
@@ -65,7 +65,7 @@ const result = isValidPassword(password);
 expect(result).toBe(true);
 });
 
-//less than 6 characters 
+//less than 6 characters - negative
 function isValidPassword(password) {
 return password.length >= 6;
 }
@@ -75,7 +75,7 @@ const result = isValidPassword(password);
 expect(result).toBe(false);
 });
 
-// more than 6 characters
+// more than 6 characters- positive
 function isValidPassword(password) {
 return password.length >= 6;
 }
@@ -85,7 +85,7 @@ const result = isValidPassword(password);
 expect(result).toBe(true);
 });
 
-// empty 
+// empty - edge case
 function isValidPassword(password) {
 return password.length >= 6;
 }
@@ -117,13 +117,101 @@ expect(result).toBe(true);
 
 
 
-
+// positive - with word
 function containsWord(sentence, word) {
 return sentence.includes(word);
-
 }
-test("checks if sentence contains word")
+test("checks if sentence contains word",()=>{
+    const sentence = "My name is Mailisha";
+    const result=containsWord(sentence, "Mailisha");
+    expect (result).toBe(true);
+})
+//negative -without the word
+function containsWord(sentence, word) {
+return sentence.includes(word);
+}
+test("checks if sentence contains word",()=>{
+    const sentence = "My name is Juliet";
+    const result=containsWord(sentence, "Mailisha");
+    expect (result).toBe(false);
+})
+
+//edge case - with different case
+function containsWord(sentence, word) {
+return sentence.includes(word);
+}
+test("checks if sentence contains word",()=>{
+    const sentence = "My name is Mailisha";
+    const result=containsWord(sentence, "mailisha");
+    expect (result).toBe(false);
+})
+
+//boundary - one letter more
+function containsWord(sentence, word) {
+return sentence.includes(word);
+}
+test("checks if sentence contains word",()=>{
+    const sentence = "My name is Mailisha";
+    const result=containsWord(sentence, "Mailishad");
+    expect (result).toBe(false);
+})
+//special character
+function containsWord(sentence, word) {
+return sentence.includes(word);
+}
+test("checks if sentence contains word",()=>{
+    const sentence = "My name is Juliet.";
+    const result=containsWord(sentence, "Juliet");
+    expect (result).toBe(true);
+})
+
+//positive
 function addItem(arr, item) {
 arr.push(item);
 return arr;
 }
+test("adds item to array",()=>{
+    const arr=[1,2,3,4];
+    const result=addItem(arr, 5);
+    expect (result).toEqual([1,2,3,4,5]);
+})
+//negative
+function addItem(arr, item) {
+arr.push(item);
+return arr;
+}
+test('removes item from array', () => {
+const arr = [1,2,3,4,5];
+const result = removeItem(arr,3);
+expect(result).toEqual([1,2,4,5]);
+});
+//edge- case
+function addItem(arr, item) {
+arr.push(item);
+return arr;
+}
+test("adds item to array",()=>{
+    const arr=[1,2,3,4];
+    const result=addItem(arr, "Juliet");
+    expect (result).toEqual([1,2,3,4,"Juliet"]);
+})
+//boundary 
+function addItem(arr, item) {
+arr.push(item);
+return arr;
+}
+test("adds item to array",()=>{
+    const arr=[];
+    const result=addItem(arr, 5);
+    expect (result).toEqual([5]);
+})
+//positive 
+function addItem(arr, item) {
+arr.push(item);
+return arr;
+}
+test("adds item to array",()=>{
+    const arr=[1,2,3,4 ];
+    const result=addItem(arr, [5,6]);
+    expect (result).toEqual([1,2,3,4,[5,6]]);
+})
